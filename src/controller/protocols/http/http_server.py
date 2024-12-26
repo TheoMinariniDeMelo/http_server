0f301b4fb4da2ba6dc2a94447bda6ad259fb0fa4
jorelwall154 <jorelwall154@gmail.com>
2024-12-26 20:36:32 -0300

    update http_server

--- src/controller/protocols/http/http_server.py
+++ src/controller/protocols/http/http_server.py
@@ -1,5 +1,13 @@
 from http_request import Request
+from http_response import Response
 
 class HttpServer: 
-    request: Request
-    
+    addr: str
+    port: int
+
+    def __init__(self, addr: str, port: int):
+        self.addr = addr
+        self.port = port
+        
+    def listen() -> None:
+        pass
