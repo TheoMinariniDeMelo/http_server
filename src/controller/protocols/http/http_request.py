285ac50cb91bd29893dc5a58adc5d4f862821e4a
jorelwall154 <jorelwall154@gmail.com>
2024-12-26 20:21:45 -0300

    update somethings

--- src/controller/protocols/http/http_request.py
+++ src/controller/protocols/http/http_request.py
@@ -1,4 +1,16 @@
-from constants import http_method;
+from constants.http_protocol import HttpProtocol;
+from constants.http_header import HttpHeader;
 
 class Request:
-    method: http_method.HttpMethod | str
+    url: str
+    protocol: HttpProtocol | str
+    header: dict[HttpHeader | str, str | int]
+
+    def get_path_params() -> list[str]:
+        pass
+
+    def get_queries_params() -> dict[str, str]:
+        pass
+
+    def get_body(body: str):
+        pass
